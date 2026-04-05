40cb451fbe41d6b05f6a0d22eeacd0f221167704
Shivanjali Gupta <shivanjaligupta7264@gmail.com>
2026-04-05 23:56:10 +0530

    ProductRepository.java

--- src/main/java/com/product/ProductRepository.java
+++ src/main/java/com/product/ProductRepository.java
@@ -1,1 +1,21 @@
 
+package com.product.repository;
+
+import com.product.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ProductRepository 
+    extends JpaRepository<Product, Integer> {
+    
+    // Find by name
+    List<Product> findByName(String name);
+    
+    // Find by price less than
+    List<Product> findByPriceLessThan(double price);
+    
+    // Find by quantity greater than
+    List<Product> findByQuantityGreaterThan(int quantity);
+}
